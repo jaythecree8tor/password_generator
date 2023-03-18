996821ece0202c6386a3ecaed91850428417ed92
Jordan Femi_Bella <femibellajordan@gmail.com>
2023-03-18 19:15:28 +0100

    styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
-import React from 'react'
-import { Navbar } from './components'
+import React from "react";
+import { Navbar } from "./components";
+import { IoCopyOutline } from "react-icons/io5";
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <div>
-        <h3>Password</h3>
-        
+      <Navbar />
+      <div className=" bg-black p-12 h-screen">
+        <div className=" z-40">
+          <h3 className=" text-white">Password</h3>
+          <button>
+            <IoCopyOutline color="white" size={20} />
+          </button>
+        </div>
+
+        <div className=" z-[100]">
+          <label className=" z-[100] text-white" htmlFor="password-strength">
+            Password Strength
+          </label>
+          <input
+            className=" z-[100]"
+            type="number"
+            id="password-strength"
+            name="password-strength"
+            max={20}
+            min={10}
+          />
+        </div>
+
+        <div className=" z-[100]">
+          <label className=" z-[100] text-white" htmlFor="password-strength">
+            Include Uppercase Letters
+          </label>
+          <input
+            className=" z-[100]"
+            type="number"
+            id="password-strength"
+            name="password-strength"
+          />
+        </div>
+
+        <div className=" z-[100]">
+          <label className=" z-[100] text-white" htmlFor="password-strength">
+            Password Strength
+          </label>
+          <input
+            className=" z-[100]"
+            type="number"
+            id="password-strength"
+            name="password-strength"
+           
+          />
+        </div>
+        <div className="absolute  w-[40%]  h-[35%] bottom-20 purple__gradient" />
+        <div className="absolute  w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { logo } from "../assets";
 const Navbar = () => {
   return (
-    <div className=" flex justify-center items-start fixed inset-x-0 bg-black py-6 px-2 w-full">
+    <div className=" flex justify-center items-start fixed inset-x-0 bg-[#00000000] py-6 px-2 w-full z-50">
       <img src={logo} alt="logo" title="home" className="w-[140px] h-[40px]"/>
     </div>
   );
